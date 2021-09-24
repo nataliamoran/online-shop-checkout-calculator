1ad03178a54af8dfa06838416058a827b919d498
RobinGerster <gersterrobyn@t-online.de>
2021-09-24 15:10:39 -0400

    navbar

--- calculator-app/src/App.js
+++ calculator-app/src/App.js
@@ -1,31 +1,15 @@
 import React from 'react';
-
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Navbar from "./components/Navbar/Navbar";
 
 import CheckoutCalc from './components/checkout-calculator';
 import './App.css';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { };
-  }
-
-  render() {
-    return (
-      <BrowserRouter>
-        <Switch>
-          <Route
-            exact
-            path="/"
-            render={() => <CheckoutCalc state={this.state} />}
-          />
-        </Switch>
-      </BrowserRouter>
-    );
-  }
+function App(){
+  return(
+      <div className={"App"}>
+        <Navbar />
+      </div>
+  )
 }
-
 export default App;
-
-//testing push
---
+++ calculator-app/src/components/Navbar/MenuItems.js
@@ -0,0 +1,28 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: '#', //Put link here
+        className: 'nav-links'
+    },
+    {
+        title: 'Store',
+        url: '#', //Put link here
+        className: 'nav-links'
+    },
+    {
+        title: 'Checkout',
+        url: '#', //Put link here
+        className: 'nav-links'
+    },
+    {
+        title: 'About',
+        url: '#', //Put link here
+        className: 'nav-links'
+    },
+    {
+        title: 'Contact Us',
+        url: '#', //Put link here
+        className: 'nav-links'
+    }
+
+]
