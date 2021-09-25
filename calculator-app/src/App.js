648cc9dffa330feb73f2a00321aa645914fcbae8
RobinGerster <gersterrobyn@t-online.de>
2021-09-25 10:25:49 -0400

    Removing Redundant Code

--- calculator-app/src/App.js
+++ calculator-app/src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+//import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Navbar from "./components/Navbar/Navbar";
 import ProductList from "./components/Products/ProductList";
 
-import CheckoutCalc from './components/checkout-calculator';
+//import CheckoutCalc from './components/checkout-calculator';
 import './App.css';
 
 function App(){
--- calculator-app/src/components/Products/ProductList.js
+++ calculator-app/src/components/Products/ProductList.js
@@ -2,7 +2,6 @@ import React from 'react'
 import {List } from 'semantic-ui-react'
 import {AddButton} from "./AddButton";
 import {RemoveButton} from "./RemoveButton";
-import item1 from '../../images/castle.jpg';
 
 const ListExampleFloated = () => (
     <List divided verticalAlign='middle'>
