40f27221befd4d87b908548ee2cf02832acb1208
RobinGerster <gersterrobyn@t-online.de>
2021-09-27 16:09:30 -0400

    More Visual Improvements

--- calculator-app/src/components/Products/ProductList.js
+++ calculator-app/src/components/Products/ProductList.js
@@ -42,24 +42,28 @@ class ProductList extends React.Component {
 
   render() {
     return (
-      <Container>
-        <Item.Group divided>
-          {this.state.items.map(item => {
-            return (
-              <Item key={item.id}>
-                <h1>{item.title}</h1>
-                <img src={require("../../images/caste.jpg").default}/>
-                <h1>{"$"+item.price}</h1>
-                <Item.Content floated='right'>
-                    <AddButton>Add</AddButton>
-                    <RemoveButton>Remove</RemoveButton>
-                </Item.Content>
-              </Item>
+  <Container>
+  <Item.Group divided>
+  {this.state.items.map(item => {
+   return (
+   <Item key={item.id}>
+      <Item.Image src={require('../../images/castle.jpg').default}  size='small' />
+      <Item.Content>
+        <Item.Header as='a'>{item.title}</Item.Header>
+        <Item.Meta>
+          <Label>{item.category.title}</Label>
+          <Label icon='dollar' content={item.price} />
+        </Item.Meta>
+        <Item.Extra>
+            <AddButton>Add</AddButton>
+            <RemoveButton>Remove</RemoveButton>
+        </Item.Extra>
+      </Item.Content>
+    </Item>);
+    })}
 
-            );
-          })}
-        </Item.Group>
-      </Container>
+  </Item.Group>
+  </Container>
     );
   }
 }
