42ac7ebc3240343a46c5cd67d6f612060c69dedc
nataliamoran <natalja.moran@gmail.com>
2021-10-03 00:10:24 -0400

    Fix CSRF issue

--- backend/calculator/urls.py
+++ backend/calculator/urls.py
@@ -1,13 +1,12 @@
 from django.urls import path, include
-from django.views.decorators.csrf import csrf_exempt
 from rest_framework import routers
 
 from . import views
 
 router = routers.DefaultRouter()
-router.register(r'products', csrf_exempt(views.ProductView), basename='products')
-router.register(r'orders', csrf_exempt(views.OrderView), basename='orders')
-router.register(r'categories', csrf_exempt(views.CategoryView), basename='categories')
+router.register(r'products', views.ProductView, basename='products')
+router.register(r'orders', views.OrderView, basename='orders')
+router.register(r'categories', views.CategoryView, basename='categories')
 
 urlpatterns = [
     path('', include(router.urls)),
